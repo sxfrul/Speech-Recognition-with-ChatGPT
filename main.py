b72a6a56e951528af8ecf0a2822076076b04c41b
sxfrul <muhdsafrul@gmail.com>
2023-09-02 23:23:17 +0800

    first code that works
    - not exactly clean code

---
+++ main.py
@@ -0,0 +1,66 @@
+import speech_recognition as sr  
+from os import system
+import openai
+
+openai.api_key = ('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')
+messages = [ {"role": "system", "content": 
+              "You are a intelligent assistant."} ]
+   
+ # obtain audio from the microphone  
+r = sr.Recognizer()  
+source = sr.Microphone()
+
+   
+def main():
+    with source:
+        system("say Please wait, calibrating microphone..")
+        r.adjust_for_ambient_noise(source, duration=1.5)
+        system("say your personal robot companion is now online")
+        while True:
+            listening = True
+            while listening:
+                audio = r.listen(source)
+                try:
+                    message = r.recognize_google(audio)
+                    message = message.lower()
+
+                    if "computer" in message:
+                        system("say Yes what do you want")
+                        listening = False
+                except:
+                    continue
+            takingInput = True
+            while takingInput: #make the led go orange
+                audio = r.listen(source)
+                try:
+                    message = r.recognize_google(audio)
+                    message = message.lower()
+                    message = ("Talk like a human and give me the simplest answers only, " + message)
+                    print(message)
+                    #make led go red
+                    system("say Please wait while i look through my database")
+                    if message:
+                        messages.append(
+                            {"role": "user", "content": message},
+                        )
+                        chat = openai.ChatCompletion.create(
+                            model="gpt-3.5-turbo", messages=messages
+                        )
+                    reply = chat.choices[0].message.content
+                    print(reply)
+                    #make led go green
+                    system("say According to my database")
+                    system('say "{}"'. format(reply))
+                    messages.append({"role": "assistant", "content": reply})
+                    takingInput = False
+                except:
+                    print("no commands given.")
+                    takingInput=False
+
+ # recognize speech using Sphinx  
+
+while True:
+    main()
+
+
+
